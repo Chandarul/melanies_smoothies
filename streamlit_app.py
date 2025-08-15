39c567eb00c07f3820918dc1592318ef799c43f3
chandarul <chandarul@gmail.com>
2025-08-15 09:57:28 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,6 @@ st.write('The name on your Smoothie will be:', name_on_order)
 my_dataframe = session.table("smoothies.public.fruit_options").select (col ('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
-st.text(smoothiefroot_response.json())
-
 
 ingredients_list = st.multiselect( 'Choose up to 5 ingredients:', my_dataframe, max_selections = 5)
 if ingredients_list:
@@ -46,3 +41,10 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
+sf_df = st.dataframe (data=smoothiefroot_response.json(), use_container_width=True)
